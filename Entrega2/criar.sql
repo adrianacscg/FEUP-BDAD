22764d464e784f5b4043b031c53904b5f5d3ae06
catarina03 <up201806610@fe.up.pt>
2020-04-01 17:01:29 +0100

    Another update

--- Entrega2/criar.sql
+++ Entrega2/criar.sql
@@ -2,19 +2,7 @@ PRAGMA foreign_keys = ON;
 
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS Country;
-CREATE TABLE Country (
-    countryInitials     TEXT NOT NULL PRIMARY KEY CHECK (length(countryInitials) = 2),
-    countryName         TEXT NOT NULL
-);
-
-DROP TABLE IF EXISTS AccountType;
-CREATE TABLE AccountType (
-    type                TEXT PRIMARY KEY CHECK(type = 'Basic' OR type = 'Standard' OR type = 'Premium'),
-    screenNumber        INTEGER NOT NULL CHECK(screenNumber < 5 AND screenNumber > 0),
-    price               REAL NOT NULL CHECK(price = 7.99 OR price = 10.99 OR price = 13.99)
-);
-
+-- ClientAccount table
 DROP TABLE IF EXISTS ClientAccount;
 CREATE TABLE ClientAccount (
     clientID            INTEGER PRIMARY KEY,
@@ -26,11 +14,29 @@ CREATE TABLE ClientAccount (
     account             TEXT REFERENCES AccountType(type) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
+-- Country table
+DROP TABLE IF EXISTS Country;
+CREATE TABLE Country (
+    countryInitials     TEXT PRIMARY KEY CHECK (length(countryInitials) = 2),
+    countryName         TEXT NOT NULL
+);
+
+-- AccountType table
+DROP TABLE IF EXISTS AccountType;
+CREATE TABLE AccountType (
+    type                TEXT PRIMARY KEY CHECK(type = 'Basic' OR type = 'Standard' OR type = 'Premium'),
+    screenNumber        INTEGER NOT NULL CHECK(screenNumber < 5 AND screenNumber > 0),
+    price               REAL NOT NULL CHECK(price = 7.99 OR price = 10.99 OR price = 13.99)
+    CHECK((type = 'Basic' AND price = 7.99) OR (type = 'Standard' AND price = 10.99) OR (type = 'Premium' AND price = 13.99))
+);
+
+-- Payment table
 DROP TABLE IF EXISTS Payment;
 CREATE TABLE Payment (
     method              TEXT PRIMARY KEY
 );
 
+-- PaymentClientAccount table
 DROP TABLE IF EXISTS PaymentClientAccount;
 CREATE TABLE PaymentClientAccount (
     method              TEXT REFERENCES Payment(method) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -38,6 +44,7 @@ CREATE TABLE PaymentClientAccount (
     PRIMARY KEY (method, clientID)
 );
 
+-- User table
 DROP TABLE IF EXISTS User;
 CREATE TABLE User (
     userID              INTEGER PRIMARY KEY,
@@ -45,6 +52,7 @@ CREATE TABLE User (
     clientID            INTEGER REFERENCES ClientAccount(clientID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- Content table
 DROP TABLE IF EXISTS Content;
 CREATE TABLE Content (
     contentID           INTEGER PRIMARY KEY,
@@ -52,9 +60,10 @@ CREATE TABLE Content (
     title               TEXT NOT NULL,
     rating              REAL,
     releaseDate         DATE NOT NULL,
-    trailer             BLOB
+    trailer             TEXT
 );
 
+-- Like table
 DROP TABLE IF EXISTS Like;
 CREATE TABLE Like (
     userID              INTEGER REFERENCES User(userID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -62,6 +71,7 @@ CREATE TABLE Like (
     PRIMARY KEY (userID, contentID)
 );
 
+-- Dislike table
 DROP TABLE IF EXISTS Dislike;
 CREATE TABLE Dislike (
     userID              INTEGER REFERENCES User(userID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -69,6 +79,7 @@ CREATE TABLE Dislike (
     PRIMARY KEY (userID, contentID)
 );
 
+-- BelongsToUserList table
 DROP TABLE IF EXISTS BelongsToUserList;
 CREATE TABLE BelongsToUserList (
     userID              INTEGER REFERENCES User(userID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -76,6 +87,7 @@ CREATE TABLE BelongsToUserList (
     PRIMARY KEY (userID, contentID)
 );
 
+-- Suggest table
 DROP TABLE IF EXISTS Suggest;
 CREATE TABLE Suggest (
     userID              INTEGER REFERENCES User(userID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -84,11 +96,13 @@ CREATE TABLE Suggest (
     PRIMARY KEY (userID, contentID)
 );
 
+-- Category table
 DROP TABLE IF EXISTS Category;
 CREATE TABLE Category (
     categoryName        TEXT PRIMARY KEY
 );
 
+-- CategoryContent table
 DROP TABLE IF EXISTS CategoryContent;
 CREATE TABLE CategoryContent (
     contentID           INTEGER REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -96,12 +110,14 @@ CREATE TABLE CategoryContent (
     PRIMARY KEY (contentID, categoryName)
 );
 
+-- Person table
 DROP TABLE IF EXISTS Person;
 CREATE TABLE Person (
     personID            INTEGER PRIMARY KEY,
     name                TEXT NOT NULL
 );
 
+-- ContentPerson table
 DROP TABLE IF EXISTS ContentPerson;
 CREATE TABLE ContentPerson (
     contentID           INTEGER REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -110,51 +126,7 @@ CREATE TABLE ContentPerson (
     PRIMARY KEY (contentID, personID)
 );
 
-DROP TABLE IF EXISTS Movie;
-CREATE TABLE Movie (
-    contentID           INTEGER PRIMARY KEY REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE,
-    contentVideo        BLOB NOT NULL,
-    duration            REAL NOT NULL CHECK(datetime(duration) > datetime('00:00'))
-);
-
-DROP TABLE IF EXISTS Series;
-CREATE TABLE Series (
-    contentID           INTEGER PRIMARY KEY REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS Season;
-CREATE TABLE Season (
-    seasonID            INTEGER PRIMARY KEY, 
-    seasonNumber        INTEGER NOT NULL CHECK(seasonNumber > 0),
-    seriesID            INTEGER NOT NULL REFERENCES Series(contentID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS Episode;
-CREATE TABLE Episode (
-    episodeID           INTEGER PRIMARY KEY,
-    title               TEXT NOT NULL,
-    duration            DATETIME NOT NULL CHECK(datetime(duration) > datetime('00:00')),
-    episodeNumber       INTEGER NOT NULL CHECK(episodeNumber > 0),
-    contentVideo        BLOB NOT NULL,
-    seasonID            INTEGER REFERENCES Season(seasonID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS Subtitles;
-CREATE TABLE Subtitles (
-    subtitleID          INTEGER PRIMARY KEY,
-    language            TEXT NOT NULL,
-    subtitlesFile       BLOB NOT NULL,
-    episodeID           INTEGER REFERENCES Episode(episodeID) ON DELETE CASCADE ON UPDATE CASCADE,
-    movieID             INTEGER REFERENCES Movie(contentID) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS Cover;
-CREATE TABLE Cover (
-    coverID             INTEGER PRIMARY KEY,
-    coverImage          BLOB NOT NULL,
-    contentID           INTEGER REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE 
-);
-
+-- Visualization table
 DROP TABLE IF EXISTS Visualization;
 CREATE TABLE Visualization (
     visualizationID     INTEGER PRIMARY KEY,
@@ -165,6 +137,57 @@ CREATE TABLE Visualization (
     movieID             INTEGER REFERENCES Movie(contentID) ON DELETE CASCADE ON UPDATE CASCADE,
     episodeID           INTEGER REFERENCES Episode(episodeID) ON DELETE CASCADE ON UPDATE CASCADE,
     CHECK(timeStampFinal > timeStampInitial)
+);
+
+-- Movie table
+DROP TABLE IF EXISTS Movie;
+CREATE TABLE Movie (
+    contentID           INTEGER PRIMARY KEY REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE,
+    contentVideo        TEXT NOT NULL,
+    duration            REAL NOT NULL CHECK(datetime(duration) > datetime('00:00'))
+);
+
+-- Series table
+DROP TABLE IF EXISTS Series;
+CREATE TABLE Series (
+    contentID           INTEGER PRIMARY KEY REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Season table
+DROP TABLE IF EXISTS Season;
+CREATE TABLE Season (
+    seasonID            INTEGER PRIMARY KEY, 
+    seasonNumber        INTEGER NOT NULL CHECK(seasonNumber > 0),
+    seriesID            INTEGER NOT NULL REFERENCES Series(contentID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Episode table
+DROP TABLE IF EXISTS Episode;
+CREATE TABLE Episode (
+    episodeID           INTEGER PRIMARY KEY,
+    title               TEXT NOT NULL,
+    duration            DATETIME NOT NULL CHECK(datetime(duration) > datetime('00:00')),
+    episodeNumber       INTEGER NOT NULL CHECK(episodeNumber > 0),
+    contentVideo        TEXT NOT NULL,
+    seasonID            INTEGER REFERENCES Season(seasonID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Subtitles table
+DROP TABLE IF EXISTS Subtitles;
+CREATE TABLE Subtitles (
+    subtitleID          INTEGER PRIMARY KEY,
+    language            TEXT NOT NULL,
+    subtitlesFile       TEXT NOT NULL,
+    episodeID           INTEGER REFERENCES Episode(episodeID) ON DELETE CASCADE ON UPDATE CASCADE,
+    movieID             INTEGER REFERENCES Movie(contentID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Cover table
+DROP TABLE IF EXISTS Cover;
+CREATE TABLE Cover (
+    coverID             INTEGER PRIMARY KEY,
+    coverImage          TEXT NOT NULL,
+    contentID           INTEGER REFERENCES Content(contentID) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
 COMMIT;
--- Entrega2/povoar.sql
+++ Entrega2/povoar.sql
@@ -1,30 +1,70 @@
  PRAGMA foreign_keys = ON;
 
 INSERT INTO Country VALUES ('PT', 'Portugal');
+INSERT INTO Country VALUES ('BR', 'Brazil');
+INSERT INTO Country VALUES ('UK', 'United Kingdom');
+INSERT INTO Country VALUES ('SP', 'Spain');
+INSERT INTO Country VALUES ('US', 'United States');
+INSERT INTO Country VALUES ('NO', 'Norway');
+INSERT INTO Country VALUES ('CA', 'Canada');
+INSERT INTO Country VALUES ('DK', 'Denmark');
+INSERT INTO Country VALUES ('SW', 'Sweden');
+INSERT INTO Country VALUES ('NL', 'Netherlands');
+INSERT INTO Country VALUES ('AS', 'Australia');
+INSERT INTO Country VALUES ('FI', 'Finland');
+INSERT INTO Country VALUES ('GM', 'Germany');
+INSERT INTO Country VALUES ('MX', 'Mexico');
+INSERT INTO Country VALUES ('AE', 'United Arab Emirates');
 
 INSERT INTO AccountType VALUES ('Basic', 1, 7.99);
 INSERT INTO AccountType VALUES ('Standard',2, 10.99);
 INSERT INTO AccountType VALUES ('Premium', 4, 13.99);
 
-INSERT INTO ClientAccount VALUES (0, '2000-04-08', 'Carlos Pereira', 'carlosa@fe.up.pt', '12345', 'PT', 'Basic');
-INSERT INTO ClientAccount VALUES (1, '2000-03-07', 'Catarina Fernandes', 'catarina@fe.up.pt', '1234', 'PT', 'Premium');
+INSERT INTO ClientAccount VALUES (0, '1995-04-08', 'Ana Silva', 'anasilva@gmail.com', '12345', 'PT', 'Standard');
+INSERT INTO ClientAccount VALUES (1, '2000-03-07', 'André Monteiro', 'andremonteiro@gmail.com', '1234', 'PT', 'Standard');
+INSERT INTO ClientAccount VALUES (2, '2002-09-22', 'Beatriz Rocha', 'beatrizrocha@gmail.com', '12345abc', 'PT', 'Premium');
+INSERT INTO ClientAccount VALUES (3, '1999-07-19', 'Ricardo Santos', 'ricardosantos@gmail.com', '000aaa', 'BR', 'Premium');
+INSERT INTO ClientAccount VALUES (4, '1980-10-10', 'Grace Smith', 'gracesmith@gmail.com', '000aaa1', 'UK', 'Basic');
 
-INSERT INTO Payment VALUES ('Cartão');
+INSERT INTO Payment VALUES ('Credit Card');
+INSERT INTO Payment VALUES ('iTunes');
+INSERT INTO Payment VALUES ('PayPal');
+INSERT INTO Payment VALUES ('Netflix Gift Card');
 
-INSERT INTO PaymentClientAccount VALUES ('Cartão', 1);
+INSERT INTO PaymentClientAccount VALUES ('iTunes', 0);
+INSERT INTO PaymentClientAccount VALUES ('Credit Card', 1);
+INSERT INTO PaymentClientAccount VALUES ('Credit Card', 2);
+INSERT INTO PaymentClientAccount VALUES ('PayPal', 3);
+INSERT INTO PaymentClientAccount VALUES ('iTunes', 4);
+INSERT INTO PaymentClientAccount VALUES ('Netflix Gift Card', 4);
 
-INSERT INTO User VALUES (0,'Victor', 1);
+INSERT INTO User VALUES (0,'Victor', 0);
+INSERT INTO User VALUES (1,'Adriana', 1);
+INSERT INTO User VALUES (2,'Catarina', 2);
+INSERT INTO User VALUES (3,'Ana', 0);
+INSERT INTO User VALUES (4,'Mariana', 0);
+INSERT INTO User VALUES (5,'André', 1);
+INSERT INTO User VALUES (6,'Beatriz', 2);
+INSERT INTO User VALUES (7,'Ricardo', 3);
+INSERT INTO User VALUES (8,'Gustavo', 3);
+INSERT INTO User VALUES (9,'Francisco', 3);
+INSERT INTO User VALUES (10,'Grace', 4);
+INSERT INTO User VALUES (11,'Phoebe', 4);
 
-INSERT INTO Content VALUES (0,'serie','Guerra dos Tronos',8.50,'2012-01-01',x'0500');
-INSERT INTO Content VALUES (1,'serie','Carbono Alterado',6.50,'2015-02-20',x'0500');
-INSERT INTO Content VALUES (2,'serie','Vikings',7.50,'2009-08-09',x'0500');
-INSERT INTO Content VALUES (3,'filme','Dollemite',8.50,'2018-05-01',x'0500');
-INSERT INTO Content VALUES (4,'filme','Velozes e Furiosos',6.50,'2010-06-20',x'0500');
-INSERT INTO Content VALUES (5,'filme','O Alto da Compadecida',7.50,'2001-06-17',x'0500');
+INSERT INTO Content VALUES (0,'Series','Guerra dos Tronos',8.50,'2012-01-01','GoT_trailer.M2TS');
+INSERT INTO Content VALUES (1,'Series','Carbono Alterado',6.50,'2015-02-20','AltCarb_trailer.M2TS');
+INSERT INTO Content VALUES (2,'Series','Vikings',7.50,'2009-08-09','Vikings_trailer.M2TS');
+INSERT INTO Content VALUES (3,'Movie','Dollemite',8.50,'2018-05-01','Dollemite_trailer.M2TS');
+INSERT INTO Content VALUES (4,'Movie','Velozes e Furiosos',6.50,'2010-06-20','FastAndFurious_trailer.M2TS');
+INSERT INTO Content VALUES (5,'Movie','O Alto da Compadecida',7.50,'2001-06-17','OAltoDaCompadecida_trailer.M2TS');
 
 INSERT INTO Like VALUES (0,5);
 INSERT INTO Like VALUES (0,0);
 INSERT INTO Like VALUES (0,3);
+INSERT INTO Like VALUES (1,0);
+INSERT INTO Like VALUES (3,0);
+INSERT INTO Like VALUES (4,0);
+INSERT INTO Like VALUES (10,0);
 
 INSERT INTO Dislike VALUES (0,4);
 
@@ -32,7 +72,8 @@ INSERT INTO BelongsToUserList VALUES (0,5);
 INSERT INTO BelongsToUserList VALUES (0,0);
 INSERT INTO BelongsToUserList VALUES (0,3);
 
-INSERT INTO Suggest VALUES (0, 2, '2020-30-3 19:19');
+INSERT INTO Suggest VALUES (0, 2, '2020-03-30 19:19');
+INSERT INTO Suggest VALUES (1, 2, '2020-03-31 18:09');
 
 INSERT INTO Category VALUES ('Comédia');
 INSERT INTO Category VALUES ('Ação');
@@ -57,17 +98,17 @@ INSERT INTO Person VALUES (6, 'Selton Mello');
 INSERT INTO Person VALUES (7, 'Denise Fraga');
 INSERT INTO Person VALUES (8, 'Virgínia Cavendish');
 
-INSERT INTO ContentPerson VALUES (2, 0, 'Lagertha');
-INSERT INTO ContentPerson VALUES (2, 1, 'Ragnar Lothbrok');
-INSERT INTO ContentPerson VALUES (2, 2, 'Bjorn Ironside');
-INSERT INTO ContentPerson VALUES (2, 3, 'FLoki');
-INSERT INTO ContentPerson VALUES (2, 4, 'Rollo');
-INSERT INTO ContentPerson VALUES (5, 5, 'João Grilo');
-INSERT INTO ContentPerson VALUES (5, 6, 'Chicó');
-INSERT INTO ContentPerson VALUES (5, 7, 'Dora');
-INSERT INTO ContentPerson VALUES (5, 8, 'Rosinha');
+INSERT INTO ContentPerson VALUES (2, 0, 'Actor');
+INSERT INTO ContentPerson VALUES (2, 1, 'Actor');
+INSERT INTO ContentPerson VALUES (2, 2, 'Actor');
+INSERT INTO ContentPerson VALUES (2, 3, 'Actor');
+INSERT INTO ContentPerson VALUES (2, 4, 'Actor');
+INSERT INTO ContentPerson VALUES (5, 5, 'Actor');
+INSERT INTO ContentPerson VALUES (5, 6, 'Actor');
+INSERT INTO ContentPerson VALUES (5, 7, 'Actor');
+INSERT INTO ContentPerson VALUES (5, 8, 'Actor');
 
-INSERT INTO Movie VALUES (5, x'0500', '02:30');
+INSERT INTO Movie VALUES (5, 'AltoDaCompadecida.M2TS', '02:30');
 
 INSERT INTO Series VALUES (0);
 INSERT INTO Series VALUES (1);
@@ -82,11 +123,11 @@ INSERT INTO Season VALUES (6, 1, 2);
 INSERT INTO Season VALUES (7, 2, 2);
 INSERT INTO Season VALUES (8, 3, 2);
 
-INSERT INTO Episode VALUES (0, 'Conto de Fadas','2013-10-07T08:23:19.120Z',1, x'0500', 0);
+INSERT INTO Episode VALUES (0, 'Conto de Fadas','00:23:19',1, 'ContoDeFadasS1E1.M2TS', 0);
 
-INSERT INTO Subtitles (subtitleID, language, subtitlesFile, episodeID) VALUES (0, 'PORTUGUÊS', x'0500', 0);
+INSERT INTO Subtitles (subtitleID, language, subtitlesFile, episodeID) VALUES (0, 'PORTUGUÊS', 'ContoDeFadasS1E1SUB.xml', 0);
 
-INSERT INTO Cover VALUES (0, x'0500', 3);
+INSERT INTO Cover VALUES (0, 'VikingsCover1.jpg', 3);
 
 INSERT INTO Visualization (visualizationID, timeOfDay, timeStampInitial, timeStampFinal, userID, movieID)
 VALUES (0,'2013-10-07T08:23:19.120Z','08:23','08:53',0,5);
