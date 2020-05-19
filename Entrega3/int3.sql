14589cb2b3079cb4e859a2213959a3036812d163
catarina03 <up201806610@fe.up.pt>
2020-05-19 18:10:23 +0100

    int3 is done

---
+++ Entrega3/int3.sql
@@ -0,0 +1,17 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+--Número total de clientes por faixa etária 
+
+SELECT ageGroup, COUNT (*) AS numberOfUsers
+FROM (SELECT * , 
+    (CASE
+        WHEN DATE('now')-DATE(dateOfBirth) < 19 THEN '0 - 18'
+        WHEN DATE('now')-DATE(dateOfBirth) >= 19 AND DATE('now')-DATE(dateOfBirth) < 26 THEN '19 - 25'
+        WHEN DATE('now')-DATE(dateOfBirth) >= 26 AND DATE('now')-DATE(dateOfBirth) < 41 THEN '26 - 40'
+        WHEN DATE('now')-DATE(dateOfBirth) >= 40 AND DATE('now')-DATE(dateOfBirth) < 61 THEN '41 - 60'
+        ELSE '61+'
+    END) AS ageGroup
+FROM ClientAccount)
+GROUP BY ageGroup;
