5b74a2d8927e7d128bb6d1ee5f5c71522945968f
catarina03 <up201806610@fe.up.pt>
2020-05-20 01:10:20 +0100

    Int1 is done

--- Entrega3/int1.sql
+++ Entrega3/int1.sql
@@ -2,35 +2,33 @@
 .headers on
 .nullvalue NULL
 
---Quais os utilizadores que já viram todos os episódios de uma série
-
---Todos os episodios agrupados por serie e temporada:
---SELECT *, COUNT(episodeID)
---FROM (Episode 
---INNER JOIN (Season INNER JOIN Series ON Season.seriesID = Series.contentID) 
---ON Episode.seasonID = Season.seasonID) 
---GROUP BY seriesID
+-- Mostra os nomes dos utlizadores que já viram todos os episódios de uma série
 
 
+CREATE VIEW [TotalEpisodesInSeries] AS
+SELECT Series.contentID, Content.title, COUNT(Series.contentID) AS totalEps
+FROM Episode 
+NATURAL JOIN Season 
+INNER JOIN Series 
+ON Season.seriesID = Series.contentID
+INNER JOIN Content 
+ON Series.contentID = Content.contentID
+GROUP BY Content.contentID;
 
 
-
---SELECT Series.contentID, Season.seasonNumber, Episode.title--, COUNT(episodeID)
-SELECT episodeID --, COUNT(episodeID)
-FROM (Episode 
-INNER JOIN (Season INNER JOIN Series ON Season.seriesID = Series.contentID) 
-ON Episode.seasonID = Season.seasonID) 
-WHERE Series.contentID = 9;
---GROUP BY seriesID
-
-INTERSECT
-
-
-SELECT episodeID
-FROM Visualization
-WHERE Visualization.userID = Visualization.userID AND Visualization.VisualizationID <> Visualization.VisualizationID;
+SELECT name
+FROM USER
+WHERE User.userID IN (
+    SELECT DISTINCT userID
+    FROM ((Visualization
+    NATURAL JOIN Episode) 
+    NATURAL JOIN Season) 
+    INNER JOIN [TotalEpisodesInSeries] T 
+    ON T.contentID = Season.seriesID 
+    WHERE Episode.episodeID IS NOT NULL 
+    GROUP BY userID, Season.seriesID
+    HAVING T.totalEps = COUNT(DISTINCT Episode.episodeID)
+);
 
 
---SELECT *
---FROM Season INNER JOIN Series 
---ON Season.seriesID = Series.contentID;
+DROP VIEW IF EXISTS [TotalEpisodesInSeries];
