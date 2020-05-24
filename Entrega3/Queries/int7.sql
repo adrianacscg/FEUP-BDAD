f4f64098a81af9e4d18c3bc279b5d5a3396b7f84
Adriana Gonçalves <adrianacscg98@gmail.com>
2020-05-24 14:42:30 +0100

    added comments to each querie

--- Entrega3/Queries/int7.sql
+++ Entrega3/Queries/int7.sql
@@ -1,6 +1,8 @@
 .mode columns
 .headers on
 
+-- média de tempo, em minutos, que cada utilizador passou a ver Netflix, por país, por mês
+
 SELECT Country.countryName AS País, strftime('%Y-%m', timeOfDay) AS Mês, AVG((cast(round((JulianDay(timestampfinal) - JulianDay(timestampinitial)) * 24 * 60) AS INTEGER))) AS Média
 FROM Visualization
 NATURAL JOIN User
