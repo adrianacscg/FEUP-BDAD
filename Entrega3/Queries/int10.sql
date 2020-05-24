51dfa10db3789d6aeb3183816310c5c8e8aae7f6
Absolf <victorlaureanocg@gmail.com>
2020-05-24 20:30:40 +0100

    proper int 10

--- Entrega3/Queries/int10.sql
+++ Entrega3/Queries/int10.sql
@@ -2,10 +2,7 @@
 .headers on
 .nullvalue NULL
 
--- Episódios deixados de lado pelos users
--- totalTime é dado em Minutos, assim como o tempo que passou a ver o episódio
+-- Filmes assistidos pelos Users
 
-SELECT userID,title AS Serie, seasonNumber, [title:1] AS epName, strftime('%H',time(duration))*60 + strftime('%M', time(duration)) AS totalTimeEpisode, cast(round((JulianDay(timestampfinal) - JulianDay(timestampinitial)) * 24 * 60) AS INTEGER) AS elapsed
-FROM seriesFullInfo, Visualization WHERE Visualization.episodeID == seriesFullInfo.seriesID AND elapsed >0 AND elapsed < totalTimeEpisode ORDER BY userID ASC;
-
+select name as Users, title AS Movies from User,Visualization JOIN Movie ON Movie.contentID = Visualization.movieID JOIN Content ON Content.contentID = Movie.contentID  WHERE User.userID = Visualization.userID ORDER BY contentVideo;
 
