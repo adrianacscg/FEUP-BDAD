d3fe46b06dcf3c4ca83219777c703ee797fa6f17
Absolf <victorlaureanocg@gmail.com>
2020-05-24 23:38:25 +0100

    verify

--- Entrega3/Triggers/gatilho2_verifica.sql
+++ Entrega3/Triggers/gatilho2_verifica.sql
@@ -12,22 +12,13 @@ ORDER BY movieRelease DESC;
 
 INSERT INTO Content VALUES (17,'Movie','Joker',10,'2019-10-03','WhySoSeriousTrailer.M2TS');
 INSERT INTO Movie VALUES (17, 'WhySoSerious.M2TS', '02:02');
+SELECT * FROM Movie;
 
 SELECT title,releaseDate AS movieRelease
 FROM Content 
 ORDER BY movieRelease DESC;
 
-<<<<<<< HEAD
-SELECT * FROM Movie;
-
-INSERT INTO Movie VALUES (17, 'WhySOSeriousJoker.M2TS', '02:40');
-
-SELECT * FROM Movie;
-
-INSERT INTO Visualization (visualizationID, timeOfDay, timeStampInitial, timeStampFinal, userID, movieID) VALUES (50, '2019-08-05 23:50', '2019-08-05 23:50', '2020-08-06 03:20', 0, 17);
-=======
 INSERT INTO Visualization (visualizationID, timeOfDay, timeStampInitial, timeStampFinal, userID, movieID) VALUES (54, '2010-08-05 23:50', '2019-08-05 23:50', '2020-08-06 03:20', 1, 17);
->>>>>>> b3be833c3a038d6027b6815ed69e2644f51c8372
 
 SELECT visualizationID,timeOfDay, movieID
 FROM Visualization 
