9c6fa170b5797bcae81e519821e0ee8e51d2d057
catarina03 <up201806610@fe.up.pt>
2020-05-24 18:28:36 +0100

    Optimized couple queries

--- Entrega3/Queries/int7.sql
+++ Entrega3/Queries/int7.sql
@@ -6,7 +6,7 @@
 SELECT Country.countryName AS País, strftime('%Y-%m', timeOfDay) AS Mês, AVG((cast(round((JulianDay(timestampfinal) - JulianDay(timestampinitial)) * 24 * 60) AS INTEGER))) AS Média
 FROM Visualization
 NATURAL JOIN User
-INNER JOIN ClientAccount ON User.clientid = Clientaccount.clientid
-INNER JOIN Country ON ClientAccount.countryInitials = Country.countryInitials
+NATURAL JOIN ClientAccount
+NATURAL JOIN Country
 GROUP BY País, Mês;
 
