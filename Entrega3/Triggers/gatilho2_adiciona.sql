b3be833c3a038d6027b6815ed69e2644f51c8372
catarina03 <up201806610@fe.up.pt>
2020-05-24 23:05:33 +0100

    Gatilho 2 is working

--- Entrega3/Triggers/gatilho2_adiciona.sql
+++ Entrega3/Triggers/gatilho2_adiciona.sql
@@ -6,9 +6,9 @@ PRAGMA foreign_keys = ON;
 
 -- Não permite que seja criado uma visualização com o dia da visualização mais antigo do que o dia de lançamento do filme
 
-CREATE TRIGGER
-EFORE INSERT ON Visualization
-WHEN strftime('%J', New.timeOfDay) > strftime('%J', (SELECT releaseDate FROM Content WHERE New.movieID = Content.contentID)) 
+CREATE TRIGGER priorVisualizatio
+BEFORE INSERT ON Visualization
+WHEN strftime('%J', New.timeOfDay) - strftime('%J', (SELECT releaseDate FROM Content WHERE New.movieID = Content.contentID)) < 0
 BEGIN
 SELECT RAISE(ROLLBACK, 'Não é possível inserir uma visualização antes do filme ser lançado!');
 END;
--- Entrega3/Triggers/gatilho2_verifica.sql
+++ Entrega3/Triggers/gatilho2_verifica.sql
@@ -4,16 +4,23 @@
 
 PRAGMA foreign_keys = ON;
 
-SELECT title,releaseDate AS movieRelease
-FROM content ORDER BY movieRelease DESC;
-
-INSERT INTO Content VALUES (17,'Movie','Joker',10,'2019-10-03','WhySoSerious.M2TS');
+-- Verifica que a data de visualização de um conteúdo é depois da data de lançamento do conteúdo
 
 SELECT title,releaseDate AS movieRelease
-FROM content ORDER BY movieRelease DESC;
+FROM Content 
+ORDER BY movieRelease DESC;
 
-INSERT INTO Visualization (visualizationID, timeOfDay, timeStampInitial, timeStampFinal, userID, movieID) VALUES (50, '2019-08-05 23:50', '2019-08-05 23:50', '2020-08-06 03:20', 1, 17);
+INSERT INTO Content VALUES (17,'Movie','Joker',10,'2019-10-03','WhySoSeriousTrailer.M2TS');
+INSERT INTO Movie VALUES (17, 'WhySoSerious.M2TS', '02:02');
+
+SELECT title,releaseDate AS movieRelease
+FROM Content 
+ORDER BY movieRelease DESC;
+
+INSERT INTO Visualization (visualizationID, timeOfDay, timeStampInitial, timeStampFinal, userID, movieID) VALUES (54, '2010-08-05 23:50', '2019-08-05 23:50', '2020-08-06 03:20', 1, 17);
 
 SELECT visualizationID,timeOfDay, movieID
-FROM Visualization ORDER BY timeOfDay DESC;
+FROM Visualization 
+WHERE movieID = 17
+ORDER BY timeOfDay DESC;
 
